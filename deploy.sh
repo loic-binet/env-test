f2e75a22ffb0f30ee3417b710bd03580a4d8e68e
loic-binet <61287923+loic-binet@users.noreply.github.com>
2021-11-07 23:57:06 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
+#deploy VM with terraform
 terraform init
 terraform validate
 terraform plan -lock=false
@@ -5,14 +6,14 @@ terraform apply -auto-approve -lock=false
 terraform plan -lock=false
 terraform apply -auto-approve -lock=false
 
+# Recovers VM IP and hostname for ansible
 file="output.txt"
 terraform output > $file
-
 ip=$(echo $(sed -n '3p' $file)| cut -d\" -f 2)
 user=$(echo $(sed -n '1p' $file)| cut -d\" -f 2)
 host=$(echo $user"@"$ip)
-
 echo $host > /etc/ansible/hosts
 rm output.txt
 
+# deploy App with Ansible
 ansible-playbook -i /etc/ansible/hosts playbook.yml
