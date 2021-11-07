94c18a2edf80bf85050b038c7dc3c882b270f816
loic-binet <61287923+loic-binet@users.noreply.github.com>
2021-11-08 00:24:36 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -12,8 +12,9 @@ terraform output > $file
 ip=$(echo $(sed -n '3p' $file)| cut -d\" -f 2)
 user=$(echo $(sed -n '1p' $file)| cut -d\" -f 2)
 host=$(echo $user"@"$ip)
-echo $host > /etc/ansible/hosts
+touch hosts
+echo $host > hosts
 rm output.txt
 
 # deploy App with Ansible
-ansible-playbook -i /etc/ansible/hosts playbook.yml
+ansible-playbook -i hosts playbook.yml
